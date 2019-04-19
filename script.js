d7927dde0fee1a174b1314d2d2dcc43f7db5e4ba
bio8oid <8bollod8@mail.co.uk>
2019-04-19 23:04:10 +0200

    Add styles

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 'use strict';
 (function(){ 
 
-window.onload=function(){
-document.getElementById('getQuote').click();
-}
 
 var prefix = "https://cors-anywhere.herokuapp.com/";
 var tweetLink = "https://twitter.com/intent/tweet?text=";
@@ -17,17 +14,17 @@ function getQuote() {
         .then(createTweet);
 }
 
-
-
 function createTweet(input) {
     var data = input[0];
-
     var dataElement = document.createElement('div');
     dataElement.innerHTML = data.content;
     var quoteText = dataElement.innerText.trim();
     var quoteAuthor = data.title;
     var tweetText = "Quote of the day - " + quoteText + " Author: " + quoteAuthor;
-
+  
+    if (!quoteAuthor.length) {
+        quoteAuthor = "Unknown author";
+    }
     if (tweetText.length > 140) {
         getQuote();
     } else {
@@ -36,46 +33,13 @@ function createTweet(input) {
         document.querySelector('.author').innerText = "Author: " + quoteAuthor;
         document.querySelector('.tweet').setAttribute('href', tweet);
     }
+}
 
-    if (!quoteAuthor.length) {
-        quoteAuthor = "Unknown author";
-    }
-
-
-    document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function() {
         getQuote();
         document.querySelector('.trigger').addEventListener('click', function() {
             getQuote();
         });
     });
-
-
-
-
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 })(); 
