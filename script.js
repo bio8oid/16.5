dfb5ce2f96dcdb8115b64bff9beb3d79ffc2af6e
bio8oid <47393762+bio8oid@users.noreply.github.com>
2019-07-12 00:49:59 +0200

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ var tweetLink = "https://twitter.com/intent/tweet?text=";
 var quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
 
 function getQuote() {
-    fetch(prefix + quoteUrl, { cache: "no-store" })
+    fetch(quoteUrl, { cache: "no-store" })
         .then(function(resp) {
             return resp.json();
         })
